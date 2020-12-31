63a707557eae1d88654d4dc10850b128c79f5c3d
lkaditya <69144603+lkaditya@users.noreply.github.com>
2020-12-31 16:21:33 +0800

    add image fetcher class and all prototyping function.... DO NOT MERGE THIS TO MASTER DIRECTLY, WILL CAUSE CONFLICT

---
+++ app/src/main/java/com/example/matchinggame/ImageFetcher.java
@@ -0,0 +1,78 @@
+package com.example.matchinggame;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ImageFetcher {
+    private String useragentValue="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0";
+    private String linkurl;
+
+    public ImageFetcher(String url){
+        this.linkurl=url;
+    }
+
+    public ImageFetcher(){
+        //linkurl="https://stocksnap.io/search/celebration+party+fireworks";
+        linkurl="https://stocksnap.io/search/cars";
+    }
+
+    public List<String> extractImage() throws MalformedURLException {
+        URL url;
+        List<String> dirty=null;
+
+        try {
+            url = new URL(linkurl);
+            URLConnection conn = url.openConnection();
+            conn.addRequestProperty("User-Agent",useragentValue);
+            // open the stream and put it into BufferedReader
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String inputLine;
+            List<String> htmlcontent= new ArrayList<String>();
+            while ((inputLine = br.readLine()) != null) {
+                htmlcontent.add(inputLine);
+            }
+            br.close();
+            dirty=htmlcontent.stream()
+                    .filter(x->x.contains("<img src="))
+                    .map(y->clearing(y))
+                    .limit(20)
+                    .collect(Collectors.toList());
+            System.out.println("size="+ dirty.size());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dirty;
+    }
+
+    public String clearing(String input){
+        return input.split("\"")[1];
+    }
+
+    public Bitmap convertImage(String url) throws IOException {
+        URL urlimg = new URL(url);
+        URLConnection conn = urlimg.openConnection();
+        conn.addRequestProperty("User-Agent", useragentValue);
+        conn.connect();
+        InputStream in = conn.getInputStream();
+        Bitmap bitmap = BitmapFactory.decodeStream(in);
+        in.close();
+        return bitmap;
+
+    }
+
+}
--- app/src/main/java/com/example/matchinggame/MainActivity.java
+++ app/src/main/java/com/example/matchinggame/MainActivity.java
@@ -1,61 +1,136 @@
 package com.example.matchinggame;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity
-{
-    private GridView gridView;
-    private List<Map<String, Object>> dataList;
-    private String[] icon = ImagesUrl.Urls;
-    private SimpleAdapter adapter;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button fetchbtn;
+    private ImageButton imgbtn;
+    private Button startbtn;
+    private ProgressBar bar;
+    private TextView msg;
+    private TextView guide;
+    private Handler handler= new Handler();
+    int status=0;
+    private final int imagetotal=8;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gridView = (GridView) findViewById(R.id.gridView);
-
-        dataList = new ArrayList<Map<String, Object>>();
-
-        adapter = new SimpleAdapter(this, getdata(), R.layout.gridview_item,
-                new String[] {"image"}, new int[] { R.id.imageView});
-
-
-
-        adapter.setViewBinder(new SimpleAdapter.ViewBinder(){
-            public boolean setViewValue(View view, Object data,
-                                        String textRepresentation) {
-                if(view instanceof ImageView){
-                    ImageView iv = (ImageView)view;
-
-                    GlideUtil.GlideWithPlaceHolder(MainActivity.this, data.toString()).into(iv);
-                    return true;
-                }else
-                    return false;
-            }
-        });
-        gridView.setAdapter(adapter);
+        fetchbtn = findViewById(R.id.fetchbtn);
+        fetchbtn.setOnClickListener(this);
+        startbtn=findViewById(R.id.start);
+        startbtn.setOnClickListener(this);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
-    private List<Map<String, Object>> getdata()
-    {
-        for(int i=0;i<icon.length;i++){
-            Map<String, Object>map=new HashMap<String, Object>();
-            map.put("image", icon[i]);
-            dataList.add(map);
+    public void storeImageInStorage(ImageFetcher im,String imgurl,File file){
+        Log.d("path", file.toString());
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            im.convertImage(imgurl).compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
         }
-        return dataList;
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == fetchbtn) {
+
+            ImageFetcher im = new ImageFetcher();
+            try {
+                List<String> imageurl = im.extractImage(); //all image downloaded
+                bar = findViewById(R.id.progress);
+                bar.setVisibility(View.VISIBLE);
+                msg = findViewById(R.id.progressmsg);
+                msg.setVisibility(View.VISIBLE);
+                //setProgressValue(0,im,imageurl,1);
+                new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+
+                        while (status<imagetotal){
+                            status++;
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    bar.setProgress(status * 10);
+                                    String mess = "Downloading " + (status) + " of "+imagetotal+" images...";
+                                    msg.setText(mess);
+                                    loadImage(im,  imageurl, status);
+                                    if(status==imagetotal){
+                                        bar.setVisibility(View.GONE);
+                                        msg.setVisibility(View.GONE);
+                                        //startbtn.setVisibility(View.VISIBLE);
+                                        guide=findViewById(R.id.guide);
+                                        guide.setVisibility(View.VISIBLE);
+                                    }
+                                }
+                            });
+                            try {Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }else if(view.getId()==R.id.img1){
+            Toast.makeText(this,"image 1 is clicked", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void loadImage(ImageFetcher im, List<String> imageurl, int x) {
+        String imgurl=imageurl.get(x-1);
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File file = new File(directory, "image" + x + ".jpg");
+        if (!file.exists()) {
+            storeImageInStorage(im,imgurl,file);
+        }
+        imgbtn = findViewById(getResources().getIdentifier("img" + x, "id", this.getPackageName()));
+        imgbtn.setOnClickListener(this);
+
+        bar.setProgress(10*x);
+        String mess="Downloading " + x + " of "+imagetotal+" images...";
+        msg.setText(mess);
+        imgbtn.setBackground(Drawable.createFromPath(file.toString()));
+
     }
 }
