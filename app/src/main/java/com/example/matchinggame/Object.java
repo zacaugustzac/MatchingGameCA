76f2ce1a7b3e3e8f9502730baf6ef06f54d2e5be
Sharon <locky.lockandkey@gmail.com>
2021-01-10 17:05:23 +0800

    Add silly Object class

---
+++ app/src/main/java/com/example/matchinggame/Object.java
@@ -0,0 +1,13 @@
+package com.example.matchinggame;
+
+public class Object {
+    String ImagesUrl;
+
+    public Object(String ImagesUrl){
+        this.ImagesUrl = ImagesUrl;
+    }
+
+    public String getImagesUrl(){
+        return ImagesUrl;
+    }
+}
